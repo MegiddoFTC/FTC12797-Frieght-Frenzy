69a2e6d3f78e79c2064687cd92e432c765703325
MegiddoFTC <59795243+MegiddoFTC@users.noreply.github.com>
2022-02-11 16:02:41 +0200

    TowerServo made

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Drive extends OpMode {
@@ -13,6 +14,9 @@ public class Drive extends OpMode {
 	DcMotor frontRight;
 
 	DcMotor towerWheel;
+	Servo towerServo;
+
+	boolean ServoUp;
 
 	@Override
 	public void init() {
@@ -22,17 +26,20 @@ public class Drive extends OpMode {
 		frontRight = hardwareMap.dcMotor.get("FrontRight");
 
 		towerWheel = hardwareMap.dcMotor.get("TowerWheel");
+		towerServo = hardwareMap.servo.get("TowerServo");
 
 		leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 		frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 		rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 		frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
 
+
 		setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 		setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 		towerWheel.setTargetPosition(0);
 		towerWheel.setDirection(DcMotorSimple.Direction.FORWARD);
+		towerWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 		towerWheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 		towerWheel.setPower(1);
 	}
@@ -50,9 +57,18 @@ public class Drive extends OpMode {
 
 		arcadeDrive(gamepad1.right_stick_x, gamepad1.left_stick_x, gamepad1.left_stick_y);
 
-		if (gamepad2.left_bumper){
-			plateRotation(1);
+		if (gamepad2.dpad_up) {
+			towerServo.setPosition(1);
+		} else if (gamepad2.dpad_down) {
+			towerServo.setPosition(0);
 		}
+		if (gamepad2.right_bumper) {
+			towerWheelSpin(1.3);
+		}
+		else if (gamepad2.left_bumper) {
+			towerWheelSpin(-1.3);
+		}
+
 	}
 
 	private void arcadeDrive(double spin, double x, double y) {
@@ -65,6 +81,12 @@ public class Drive extends OpMode {
 	private void plateRotation(double rotations) {
 		towerWheel.setTargetPosition(towerWheel.getCurrentPosition()
 				+ (int)(rotations * Constants.TowerConstants.ticks_per_revolution));
+	}
+
+	private void towerWheelSpin(double rotation) {
+		if (towerServo.getPosition() == 1) {
+			towerWheel.setTargetPosition(towerWheel.getCurrentPosition() + (int)(rotation * Constants.TowerConstants.ticks_per_revolution));
+		}
 	}
 
 }
